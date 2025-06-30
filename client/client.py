45f1dd970bf2a0f5ef83eef8a181ab66a652a654
noufa03 <snoufa3@gmail.com>
2025-06-30 11:52:33 +0530

    changes

--- client/client.py
+++ client/client.py
@@ -6,16 +6,17 @@ def client(server_ip,port):
     c.connect((server_ip,port))
   
     while True:
-        msg=input('Enter your Message to server or terminate (if you want to close the connection):\n')
+        msg=input('Enter your Message to server or terminate (if you want to close the connection):\n You:')
         while msg.lower() != 'terminate':
             c.send(bytes(msg,'utf-8'))
-            print(c.recv(1024).decode()) 
+            print('Server:',c.recv(1024).decode()) 
             
         print("Terminating,client.....")
         c.send(bytes(msg,'utf-8'))
         c.close()
         
 if __name__ == "__main__":
+
     server_ip=sys.argv[1]
     port=int(sys.argv[2])
     
